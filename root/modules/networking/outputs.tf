b81478db9d03516cca70c1ab1ac1033ceb24f8c9
olaf-sandberg <sandberg.olaf@gmail.com>
2025-04-05 16:23:44 +0200

    outputs removed for testing

--- root/modules/networking/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.this.id
-}
-
-output "subnet_cidr" {
-    description = "CIDR of sunets"
-  value       = aws_subnet.name.id
-}
--- root/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "prod_vpc_id" {
-  value = module.prod_vpc.vpc_id
-}
-
-output "lab_vpc_id" {
-  value = module.prod_vpc.vpc_id
-}
