cae86d7a41b2e4f377d39ce520e458b545920862
olaf-sandberg <sandberg.olaf@gmail.com>
2025-04-05 09:51:59 +0200

    sample changes to root vars

--- root/outputs.tf
+++ root/outputs.tf
@@ -1,34 +1,7 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
+output "prod_vpc_id" {
+  value = module.prod_vpc.vpc_id
 }
 
-output "public_subnet_id" {
-  description = "ID of the public subnet"
-  value       = aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  description = "ID of the private subnet"
-  value       = aws_subnet.private.id
-}
-
-output "internet_gateway_id" {
-  description = "ID of the Internet Gateway"
-  value       = aws_internet_gateway.igw.id
-}
-
-output "public_route_table_id" {
-  description = "ID of the public route table"
-  value       = aws_route_table.public_rt.id
-}
-
-output "public_sg_id" {
-  description = "ID of the public security group"
-  value       = aws_security_group.public_sg.id
-}
-
-output "private_sg_id" {
-  description = "ID of the private security group"
-  value       = aws_security_group.private_sg.id
+output "lab_vpc_id" {
+  value = module.prod_vpc.vpc_id
 }
